859a6c592c0d54fd3e404aa95977b358b45cdb8a
Viviane <vivianedias@protonmail.com>
2020-08-09 19:41:52 -0300

    feat(modal): adds new modal component

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@ export { default as Header } from './content/Header';
 export { default as Link } from './content/Link';
 export { default as Question } from './content/Question';
 export { default as Shortcut } from './content/Shortcut';
+export { default as Modal } from './content/Modal';
+export { default as CleanButton } from './content/CleanButton';
 
 export { default as Dropdown } from './dropdown/Dropdown';
 export { default as DropdownIconItem } from './dropdown/DropdownIconItem';
