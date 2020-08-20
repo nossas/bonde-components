c5bac7232c585dd123555b53f2020b841c53b4c0
Viviane <vivianedias@protonmail.com>
2020-08-20 18:58:34 -0300

    feat(pagination): adds pagination component

---
+++ src/content/Pagination.tsx
@@ -0,0 +1,218 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Button from './Button';
+import RoundSelect from '../form/RoundSelect';
+import theme from '../base/theme';
+
+type Props = {
+  goToPage: (arg: number) => void;
+  pageCount: number;
+  pageIndex: number;
+  pageSize: number;
+  setPageSize: (arg: number) => void;
+  totalPages: number;
+};
+
+const WrapPagination = styled.div`
+  display: grid;
+  margin: 20px 0px;
+  grid-template-columns: 40px 80px minmax(100px, 650px) 80px 40px 100px;
+  grid-gap: 15px;
+`;
+
+const WrapItems = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+`;
+
+const StyledButton = styled(Button)<{
+  disabled: boolean;
+  active: boolean;
+  theme: any;
+}>`
+  &:focus,
+  &:active {
+    color: ${({ theme }) => theme.dark.color.main};
+  }
+  ${({ disabled, theme }) =>
+    disabled &&
+    `
+    background: none;
+    color: ${theme.commons.main};
+  `}
+  ${({ active, theme }) =>
+    active &&
+    `
+    color: ${theme.brand.main};
+    &:focus, &:active {
+      color: ${theme.brand.main};
+    }
+  `}
+`;
+
+StyledButton.defaultProps = {
+  theme,
+};
+
+const WrapSelect = styled.div<{ theme: any }>`
+  & .Select__control {
+    border-color: ${({ theme }) => theme.brand.dark};
+    &:hover {
+      border-color: ${({ theme }) => theme.commons.dark};
+    }
+    &:focus {
+      border-color: ${({ theme }) => theme.dark.border.focus};
+    }
+  }
+  & .Select__single-value {
+    color: #000;
+    &:hover {
+      color: ${({ theme }) => theme.commons.dark};
+    }
+    &:focus {
+      color: ${({ theme }) => theme.dark.border.focus};
+    }
+  }
+  .Select__indicators svg path {
+    fill: #000;
+    &:hover {
+      fill: ${({ theme }) => theme.commons.dark};
+    }
+    &:focus {
+      fill: ${({ theme }) => theme.dark.border.focus};
+    }
+  }
+`;
+
+WrapSelect.defaultProps = {
+  theme,
+};
+
+const Pagination = ({
+  goToPage,
+  pageCount,
+  pageIndex,
+  pageSize,
+  totalPages,
+  setPageSize,
+}: Props): React.ReactElement => {
+  const [items, setItems] = useState<number[]>([]);
+
+  const getItems = (page: number, totalPages: number) => {
+    let startPage: number, endPage: number;
+    if (totalPages <= 10) {
+      // less than 10 total pages so show all
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      // more than 10 total pages so calculate start and end pages
+      if (page <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (page + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = page - 5;
+        endPage = page + 4;
+      }
+    }
+
+    // create an array of pages to ng-repeat in the pager control
+    const pages = Array.from(
+      Array(endPage + 1 - startPage),
+      (_, i) => startPage + i
+    );
+
+    return setItems(pages);
+  };
+
+  const setPage = (page: number, totalPages: number) => {
+    getItems(page, totalPages);
+    return goToPage(page);
+  };
+
+  useEffect(() => {
+    return getItems(0, totalPages);
+  }, []);
+
+  return (
+    <WrapPagination>
+      <StyledButton
+        secondary
+        onClick={() => setPage(0, totalPages)}
+        disabled={pageIndex === 0}
+      >
+        {'<<'}
+      </StyledButton>{' '}
+      <StyledButton
+        secondary
+        onClick={() => setPage(pageIndex - 1, totalPages)}
+        disabled={pageIndex === 0}
+      >
+        anterior
+      </StyledButton>{' '}
+      <WrapItems>
+        {items.map((item, i) => (
+          <StyledButton
+            active={item - 1 === pageIndex}
+            secondary
+            onClick={() => setPage(item - 1, totalPages)}
+            key={`page-item-${i}`}
+          >
+            {item}
+          </StyledButton>
+        ))}
+      </WrapItems>
+      <StyledButton
+        secondary
+        onClick={() => setPage(pageIndex + 1, totalPages)}
+        disabled={pageIndex === pageCount - 1}
+      >
+        próxima
+      </StyledButton>{' '}
+      <StyledButton
+        secondary
+        onClick={() => setPage(pageCount - 1, totalPages)}
+        disabled={pageIndex === pageCount - 1}
+      >
+        {'>>'}
+      </StyledButton>{' '}
+      <WrapSelect>
+        <RoundSelect
+          options={[
+            {
+              value: 10,
+              label: '10',
+            },
+            {
+              value: 20,
+              label: '20',
+            },
+            {
+              value: 30,
+              label: '30',
+            },
+            {
+              value: 40,
+              label: '40',
+            },
+            {
+              value: 50,
+              label: '50',
+            },
+          ]}
+          placeholder=""
+          value={{
+            value: pageSize,
+            label: `Mostrar ${pageSize}`,
+          }}
+          onChange={e => setPageSize(Number(e.value))}
+          menuPortalTarget={document.querySelector('body')}
+        />
+      </WrapSelect>
+    </WrapPagination>
+  );
+};
+
+export default Pagination;
