c566d959ae49c1223b18709d7cde3b727bc0629f
Viviane Dias <vivipirolad@gmail.com>
2020-09-04 17:38:19 -0300

    feat(question): update question to have a more descriptive props

--- src/content/Question.tsx
+++ src/content/Question.tsx
@@ -10,10 +10,11 @@ type Props = {
     };
     params: Record<string, number | string>;
   };
-  questionOptions?: string;
+  border: boolean;
+  title: boolean;
 };
 
-const Question = ({ siteUrl, secretKey, config, questionOptions }: Props) => {
+const Question = ({ siteUrl, secretKey, config, border, title }: Props) => {
   const payload = {
     ...config,
     exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
@@ -21,7 +22,8 @@ const Question = ({ siteUrl, secretKey, config, questionOptions }: Props) => {
 
   const token = jwt.sign(payload, secretKey);
 
-  const iframeUrl = siteUrl + '/embed/question/' + token + questionOptions;
+  const iframeUrl =
+    siteUrl + '/embed/question/' + token + `bordered=${border}&titled=${title}`;
 
   return (
     <iframe
@@ -30,6 +32,13 @@ const Question = ({ siteUrl, secretKey, config, questionOptions }: Props) => {
       style={{ border: 'none', width: '100%', height: '100%' }}
     />
   );
+};
+
+Question.defaultProps = {
+  siteUrl: process.env.REACT_APP_METABASE_SITE_URL || '',
+  secretKey: process.env.REACT_APP_METABASE_SECRET_KEY || '',
+  border: false,
+  title: false,
 };
 
 export default Question;
