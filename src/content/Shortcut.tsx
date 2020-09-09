973ab631f666d4b64bf4b0a1b44e08b76dea4db1
Viviane Dias <vivipirolad@gmail.com>
2020-09-09 17:59:06 -0300

    feat(shortcut): add white color to background

--- src/content/Shortcut.tsx
+++ src/content/Shortcut.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from './Header';
+import theme from '../base/theme';
 
 const CleanButton = styled.button`
   /* Reset Button CSS */
@@ -26,12 +27,14 @@ const CleanButton = styled.button`
   /* Corrects inability to style clickable 'input' types in iOS */
   -webkit-appearance: none;
 `;
-const Wrapper = styled(CleanButton)`
+
+const Wrapper = styled(CleanButton)<{ theme: any }>`
   width: 200px;
   height: 90px;
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
   cursor: pointer;
+  background-color: ${({ theme }) => theme.brand.light};
 
   display: grid;
   grid-template-columns: auto auto;
@@ -51,11 +54,23 @@ const Text = styled(Header.H5)`
   text-align: left;
 `;
 
-const Shortcut = ({ icon, text }: { icon: any; text: string }) => (
-  <Wrapper>
+const Shortcut = ({
+  icon,
+  text,
+  theme,
+}: {
+  icon: React.ReactElement;
+  text: string;
+  theme: any;
+}) => (
+  <Wrapper theme={theme}>
     {icon}
     <Text>{text}</Text>
   </Wrapper>
 );
+
+Shortcut.defaultProps = {
+  theme,
+};
 
 export default Shortcut;
