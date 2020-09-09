012e3191842acad2450aeb58a00baf4545234d7f
Viviane Dias <vivipirolad@gmail.com>
2020-09-09 17:53:07 -0300

    feat(question): add a spinner to question if iframe isnt yet loaded

--- src/content/Question.tsx
+++ src/content/Question.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import jwt from 'jsonwebtoken';
+import Spinner from '../await/Spinner';
+import styled from 'styled-components';
+
+const Center = styled.div`
+  display: flex;
+`;
 
 type Props = {
   siteUrl: string;
@@ -15,6 +21,8 @@ type Props = {
 };
 
 const Question = ({ siteUrl, secretKey, config, border, title }: Props) => {
+  const [isLoading, setLoader] = useState(true);
+
   const payload = {
     ...config,
     exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
@@ -29,11 +37,19 @@ const Question = ({ siteUrl, secretKey, config, border, title }: Props) => {
     `#bordered=${border}&titled=${title}`;
 
   return (
-    <iframe
-      src={iframeUrl}
-      title="Metabase"
-      style={{ border: 'none', width: '100%', height: '100%' }}
-    />
+    <>
+      {isLoading && (
+        <Center>
+          <Spinner />
+        </Center>
+      )}
+      <iframe
+        onLoad={() => setLoader(false)}
+        src={iframeUrl}
+        title="Metabase"
+        style={{ border: 'none', width: '100%', height: '100%' }}
+      />
+    </>
   );
 };
 
