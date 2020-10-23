d9acc7a03e8cf771fcc43b3395c53168d4072b5a
Viviane Dias <vivianedias@protonmail.com>
2020-10-23 19:07:40 -0300

    feat(SubHeader): update SubHeader to wrap Header/Navigation

--- src/content/SubHeader.tsx
+++ src/content/SubHeader.tsx
@@ -3,13 +3,16 @@ import Header from './Header';
 
 const SubHeader = styled.div`
   display: flex;
-  align-items: center;
-  height: 50px;
+  flex-direction: column;
   background-color: #000;
   padding: 0 60px;
 
   ${Header.H3} {
     color: #fff;
+    margin: 0;
+    height: 50px;
+    display: inherit;
+    align-items: center;
   }
 `;
 
