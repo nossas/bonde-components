e5fed44e98a6360f24b2aa069058509e3893efb1
Igor Santos <igr.exe@gmail.com>
2020-11-03 16:17:59 -0300

    feat: add Tooltip component

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,7 @@ export { default as Table } from './content/Table';
 export { default as Pagination } from './content/Pagination';
 export { default as SubHeader } from './content/SubHeader';
 export { default as Card } from './content/Card';
+export { default as Tooltip } from './content/Tooltip';
 
 export { default as Dropdown } from './dropdown/Dropdown';
 export { default as DropdownIconItem } from './dropdown/DropdownIconItem';
