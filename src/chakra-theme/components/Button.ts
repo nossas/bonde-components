5c1863deeeff666475d739d650978e415899c0fe
Igor Santos <igr.exe@gmail.com>
2021-08-31 11:55:13 -0300

    feat: customize px to Button sizes

--- src/chakra-theme/components/Button.ts
+++ src/chakra-theme/components/Button.ts
@@ -1,3 +1,8 @@
+import type {
+  SystemStyleObject,
+  SystemStyleFunction,
+} from "@chakra-ui/theme-tools"
+
 type Props = {
   colorScheme: "pink" | "gray"
 }
@@ -17,92 +22,96 @@ const disabled = {
   }
 }
 
+const sizes: Record<string, SystemStyleObject> = {
+  lg: {
+    px: 8
+  },
+  md: {
+    px: 6
+  },
+  sm: {
+    px: 4
+  },
+  sx: {
+    px: 3
+  }
+}
+
+const variants: Record<string, SystemStyleObject | SystemStyleFunction> = {
+  solid: ({ colorScheme }: Props): any => ({
+    bg: `${colorScheme}.100`,
+    color: "white",
+    _hover: {
+      bg: `${colorScheme}.200`,
+      _disabled: disabled.solid
+    },
+    _active: {
+      bg: `${colorScheme}.300`,
+      _disabled: disabled.solid
+    },
+    _focus: {
+      boxShadow: "none",
+      _disabled: disabled.solid
+    },
+    _disabled: disabled.solid
+  }),
+  outline: ({ colorScheme }: Props): any => ({
+    bg: "transparent",
+    color: `${colorScheme}.300`,
+    borderColor: `${colorScheme}.300`,
+    _hover: {
+      color: `${colorScheme}.200`,
+      borderColor: `${colorScheme}.200`,
+      bg: "transparent",
+      _disabled: disabled.outline
+    },
+    _active: {
+      color: `${colorScheme}.100`,
+      borderColor: `${colorScheme}.100`,
+      bg: "transparent",
+      _disabled: disabled.outline
+    },
+    _focus: {
+      boxShadow: "none"
+    },
+    _disabled: disabled.outline
+  }),
+  link: ({ colorScheme }: Props): any => ({
+    bg: "transparent",
+    borderColor: "transparent",
+    color: `${colorScheme}.300`,
+    minW: "auto",
+    _hover: {
+      color: `${colorScheme}.200`,
+      borderColor: "transparent",
+      bg: "transparent",
+      textDecoration: "none",
+      _disabled: disabled.link
+    },
+    _active: {
+      color: `${colorScheme}.100`,
+      borderColor: "transparent",
+      bg: "transparent",
+      textDecoration: "none",
+      _disabled: disabled.link
+    },
+    _focus: {
+      boxShadow: "none"
+    },
+    _disabled: disabled.link
+  })
+}
+
 export default {
   baseStyle: {
     borderRadius: "50px",
     fontWeight: "800",
     textTransform: "uppercase"
   },
-  variants: {
-    solid: ({ colorScheme }: Props): any => ({
-      bg: `${colorScheme}.100`,
-      color: "white",
-      _hover: {
-        bg: `${colorScheme}.200`,
-        _disabled: disabled.solid
-      },
-      _active: {
-        bg: `${colorScheme}.300`,
-        _disabled: disabled.solid
-      },
-      _focus: {
-        boxShadow: "none",
-        _disabled: disabled.solid
-      },
-      _disabled: disabled.solid
-    }),
-    outline: ({ colorScheme }: Props): any => ({
-      bg: "transparent",
-      color: `${colorScheme}.300`,
-      borderColor: `${colorScheme}.300`,
-      _hover: {
-        color: `${colorScheme}.200`,
-        borderColor: `${colorScheme}.200`,
-        bg: "transparent",
-        _disabled: disabled.outline
-      },
-      _active: {
-        color: `${colorScheme}.100`,
-        borderColor: `${colorScheme}.100`,
-        bg: "transparent",
-        _disabled: disabled.outline
-      },
-      _focus: {
-        boxShadow: "none"
-      },
-      _disabled: disabled.outline
-    }),
-    link: ({ colorScheme }: Props): any => ({
-      bg: "transparent",
-      borderColor: "transparent",
-      color: `${colorScheme}.300`,
-      minW: "auto",
-      _hover: {
-        color: `${colorScheme}.200`,
-        borderColor: "transparent",
-        bg: "transparent",
-        textDecoration: "none",
-        _disabled: disabled.link
-      },
-      _active: {
-        color: `${colorScheme}.100`,
-        borderColor: "transparent",
-        bg: "transparent",
-        textDecoration: "none",
-        _disabled: disabled.link
-      },
-      _focus: {
-        boxShadow: "none"
-      },
-      _disabled: disabled.link
-    })
-  },
-  sizes: {
-    xs: {
-      h: "42px",
-      fontSize: "sm",
-      px: 4
-    },
-    sm: {
-      h: "42px",
-      minW: "192px",
-      fontSize: "sm",
-      px: 4
-    },
-  },
+  variants,
+  sizes,
   defaultProps: {
     variant: "solid",
-    colorScheme: "pink",
-    size: "sm"
+    colorScheme: "pink"
   }
 }
