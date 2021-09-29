3a4f4892ed92e849e775806ea6f7ced0e2d930ed
Igor Santos <igr.exe@gmail.com>
2021-09-29 15:02:06 -0300

    feat: add ChartIcon

--- src/index.tsx
+++ src/index.tsx
@@ -71,6 +71,7 @@ export { default as FontsLoader } from './chakra-theme/FontsLoader';
 export { default as ArrowUpIcon } from './chakra-theme/icons/ArrowUpIcon';
 export { default as ArrowDownIcon } from './chakra-theme/icons/ArrowDownIcon';
 export { default as BotIcon } from './chakra-theme/icons/BotIcon';
+export { default as ChartIcon } from './chakra-theme/icons/ChartIcon';
 export { default as EditIcon } from './chakra-theme/icons/EditIcon';
 export { default as InfoIcon } from './chakra-theme/icons/InfoIcon';
 export { default as NetIcon } from './chakra-theme/icons/NetIcon';
