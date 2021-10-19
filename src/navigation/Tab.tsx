c8998207a3103c6d588f89f94da22cfbfd9439bc
Igor Santos <igr.exe@gmail.com>
2021-10-19 09:20:04 -0300

    fix: spacing tabs and container

--- src/navigation/Tab.tsx
+++ src/navigation/Tab.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
-import { useColorMode } from "@chakra-ui/color-mode";
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+import { useColorMode } from '@chakra-ui/color-mode';
 
 export interface TabProps {
-  children: any
-  active?: boolean
-  onClick?: () => void
+  children: any;
+  active?: boolean;
+  onClick?: () => void;
 }
 
 const Tab: React.FC<TabProps> = ({ children, active, onClick }) => {
   const { colorMode } = useColorMode();
-  const defaultColor = colorMode === "dark" ? "white" : "black";
-  
+  const defaultColor = colorMode === 'dark' ? 'white' : 'black';
+
   const defaultStylesProps: any = {
-    color: defaultColor
+    color: defaultColor,
   };
-  if (colorMode === "dark" && active) {
-    defaultStylesProps.borderBottomWidth = "2px";
-    defaultStylesProps.borderBottomStyle = "solid";
-    defaultStylesProps.borderBottomColor = "pink.200";
+  if (colorMode === 'dark' && active) {
+    defaultStylesProps.borderBottomWidth = '2px';
+    defaultStylesProps.borderBottomStyle = 'solid';
+    defaultStylesProps.borderBottomColor = 'pink.200';
   }
   if (active) {
-    defaultStylesProps.color = "pink.200"
+    defaultStylesProps.color = 'pink.200';
     defaultStylesProps._hover = {
-      color: "pink.200",
-      borderBottomColor: "pink.200"
-    }
+      color: 'pink.200',
+      borderBottomColor: 'pink.200',
+    };
   }
 
   return (
@@ -34,12 +34,13 @@ const Tab: React.FC<TabProps> = ({ children, active, onClick }) => {
       colorScheme="gray"
       onClick={onClick}
       px={0}
+      pb={2}
       mr={5}
       {...defaultStylesProps}
     >
       {children}
     </Button>
   );
-}
+};
 
 export default Tab;
