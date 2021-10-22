9ec4b47f112a11ae54ea4398b1eca81001a799cf
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-10-22 09:01:36 -0300

    Revert "fix: fix lint errors"
    This reverts commit 7d6c1a1fd392d5a69899724a3ac55aa90fd0bf04.

--- src/chakra-theme/components/Button.ts
+++ src/chakra-theme/components/Button.ts
@@ -1,101 +1,101 @@
-import {
+import type {
   StyleFunctionProps,
   SystemStyleObject,
   SystemStyleFunction,
-} from '@chakra-ui/theme-tools';
+} from "@chakra-ui/theme-tools";
 
 const disabled = {
   solid: {
-    bg: 'gray.100',
-    color: 'white',
+    bg: "gray.100",
+    color: "white"
   },
   outline: {
-    color: 'gray.100',
-    borderColor: 'gray.100',
-    bg: 'transparent',
+    color: "gray.100",
+    borderColor: "gray.100",
+    bg: "transparent"
   },
   link: {
-    color: 'gray.100',
-  },
-};
+    color: "gray.100"
+  }
+}
 
 const sizes: Record<string, SystemStyleObject> = {
   lg: {
-    px: 8,
+    px: 8
   },
   md: {
-    px: 6,
+    px: 6
   },
   sm: {
-    px: 4,
+    px: 4
   },
   sx: {
-    px: 3,
-  },
-};
+    px: 3
+  }
+}
 
 const variants: Record<string, SystemStyleObject | SystemStyleFunction> = {
   solid: ({ colorScheme }: StyleFunctionProps): any => ({
     bg: `${colorScheme}.200`,
-    color: 'white',
+    color: "white",
     _hover: {
       bg: `${colorScheme}.300`,
-      _disabled: disabled.solid,
+      _disabled: disabled.solid
     },
     _active: {
       bg: `${colorScheme}.400`,
-      _disabled: disabled.solid,
+      _disabled: disabled.solid
     },
     _focus: {
-      boxShadow: 'none',
-      _disabled: disabled.solid,
+      boxShadow: "none",
+      _disabled: disabled.solid
     },
-    _disabled: disabled.solid,
+    _disabled: disabled.solid
   }),
   outline: ({ colorScheme }: StyleFunctionProps): any => ({
-    bg: 'transparent',
+    bg: "transparent",
     color: `${colorScheme}.400`,
     borderColor: `${colorScheme}.400`,
     _hover: {
       color: `${colorScheme}.300`,
       borderColor: `${colorScheme}.300`,
-      bg: 'transparent',
-      _disabled: disabled.outline,
+      bg: "transparent",
+      _disabled: disabled.outline
     },
     _active: {
       color: `${colorScheme}.200`,
       borderColor: `${colorScheme}.200`,
-      bg: 'transparent',
-      _disabled: disabled.outline,
+      bg: "transparent",
+      _disabled: disabled.outline
     },
     _focus: {
-      boxShadow: 'none',
+      boxShadow: "none"
     },
-    _disabled: disabled.outline,
+    _disabled: disabled.outline
   }),
   link: ({ colorScheme }: StyleFunctionProps): any => ({
-    bg: 'transparent',
-    borderColor: 'transparent',
+    bg: "transparent",
+    borderColor: "transparent",
     color: `${colorScheme}.400`,
-    minW: 'auto',
+    minW: "auto",
     _hover: {
       color: `${colorScheme}.300`,
-      borderColor: 'transparent',
-      bg: 'transparent',
-      textDecoration: 'none',
-      _disabled: disabled.link,
+      borderColor: "transparent",
+      bg: "transparent",
+      textDecoration: "none",
+      _disabled: disabled.link
     },
     _active: {
       color: `${colorScheme}.200`,
-      borderColor: 'transparent',
-      bg: 'transparent',
-      textDecoration: 'none',
-      _disabled: disabled.link,
+      borderColor: "transparent",
+      bg: "transparent",
+      textDecoration: "none",
+      _disabled: disabled.link
     },
     _focus: {
-      boxShadow: 'none',
+      boxShadow: "none"
     },
-    _disabled: disabled.link,
+    _disabled: disabled.link
   }),
   tableLink: ({ colorScheme }: StyleFunctionProps): any => ({
     bg: 'transparent',
@@ -122,33 +122,33 @@ const variants: Record<string, SystemStyleObject | SystemStyleFunction> = {
     _disabled: disabled.link,
   }),
   dropdown: ({ color }: StyleFunctionProps): any => ({
-    bg: 'transparet',
-    borderColor: 'trasparent',
+    bg: "transparet",
+    borderColor: "trasparent",
     color,
-    minW: 'auto',
+    minW: "auto",
     _hover: {
-      color,
+      color
     },
     _active: {
-      color,
+      color
     },
     _focus: {
-      boxShadow: 'none',
+      boxShadow: "none"
     },
-  }),
-};
+  })
+}
 
 export default {
   baseStyle: {
-    borderRadius: '50px',
-    fontWeight: '800',
-    textTransform: 'uppercase',
+    borderRadius: "50px",
+    fontWeight: "800",
+    textTransform: "uppercase"
   },
   variants,
   sizes,
   defaultProps: {
-    variant: 'solid',
-    colorScheme: 'pink',
-    size: 'sm',
-  },
-};
+    variant: "solid",
+    colorScheme: "pink",
+    size: "sm"
+  }
+}
